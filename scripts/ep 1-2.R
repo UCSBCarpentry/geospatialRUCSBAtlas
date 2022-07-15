33d78c745ad451583e39e55a575b0085582af81f
jonjab <jonjab@ucsb.edu>
2022-07-15 16:21:30 -0700

    friday map coding. little accomplished.

--- scripts/ep 1-2.R
+++ scripts/ep 1-2.R
@@ -12,75 +12,115 @@ library(raster)
 library(rgdal)
 library(RColorBrewer)
 
-setwd("C:/users/your_file_path")
+# setwd("C:/users/your_file_path")
 
 #############################################
 # ep. 1
 
-# get your first raster dataset
+# get info about your first raster dataset
 GDALinfo("source_data/greatercampusDEM/greatercampusDEM_1_1.tif")
 
+# make it into an object you can manipulate
 campus_DEM <- raster("source_data/greatercampusDEM/greatercampusDEM_1_1.tif")
 
 # run the object, units are in feet
+# 5 feet x 5 feet pixels
 campus_DEM
 
 # get summary of object, min and max make sense for UCSB
 summary(campus_DEM)
 
-# can force it to calculate on all
+# we know there's a deeper hole than .1 feet, so...
+# can force it to calculate on all pixels
 summary(campus_DEM, maxsamp = ncell(campus_DEM))
 
 # or do that the tidy way with pipes
+# and get different output
 campus_DEM %>% 
   ncell() %>% 
   summary()
 
 # summary plots require a dataframe
 campus_DEM_df <- as.data.frame(campus_DEM, xy = TRUE)
+
+# both summmaries and str of the dataframe
+# show you the name of the layer you'll need to 
+# refer to.
+
 str(campus_DEM_df)
 
-
-# the "greatercampusDEM_1_1" column name is an artifact
-# of Jon's poor file naming scheme when creating the data.
-
 ggplot() +
-  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = greatercampusDEM_1_1)) +
+  geom_raster(data = campus_DEM_df, 
+              aes(x=x, y=y, fill = greatercampusDEM_1_1)) +
   scale_fill_viridis_c() +
   coord_quickmap()
 
 # faster base R plot
+# also doesn't force you to remember the name of
+# the data layer
 plot(campus_DEM_df)
+
+
+# that's still slow as molasses, 
+# for instructional purposes, we need to downsample
+# so our maps draw faster
+# see episode 9 for 'aggregate'
+campus_DEM_downsampled <- aggregate(campus_DEM, fact = 4)
+
+# let's rename the column so we don't have to keep typing
+# greatercampusDEM_1_1
+campus_DEM_df <- as.data.frame(campus_DEM_downsampled, xy=TRUE) %>% 
+  rename(altitude = greatercampusDEM_1_1)
+
+
+
+# this one doesn't work out. so leave it behind.
+#zoom_DEM <- raster("source_data/greatercampusDEM/DEMmosaic.tif")
+#plot(zoom_DEM)
+
+#zoom_df <- as.data.frame(zoom_DEM, xy=TRUE)
+#ggplot() +
+#  geom_raster(data = zoom_df, 
+#              aes(x=x, y=y, fill = DEMmosaic)) +
+#  scale_fill_viridis_c() +
+#  coord_quickmap()
 
 
 # deal with no data
 ggplot() +
-  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = greatercampusDEM_1_1)) +
+  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = altitude)) +
   scale_fill_viridis_c(na.value = "deeppink") +
   coord_quickmap()
 
-# lesson example hightlights Harvard Forest pixels > 400m.
-# for us, let's highlight below sea level.
 
+# lesson example highlights Harvard Forest pixels > 400m.
+# for us, let's highlight below 1m above sea level.
 ggplot() +
-  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = greatercampusDEM_1_1)) +
+  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = altitude)) +
   scale_fill_gradient2(na.value = "lightgrey", 
-                       low="black", 
-                       mid="azure1", 
+                       low="red", 
+                       mid="white", 
                        high="cornsilk3",
-                       midpoint=0) +
+                       guide = "colourbar",
+                        midpoint = 3.12, aesthetics = "fill") +
   coord_quickmap()
 
-# looking at overview of values again
+# we can't see them, because there are too few.
+# how few?
 summary(campus_DEM_df)
-test <- sum(campus_DEM_df$greatercampusDEM_1_1 <= 0)
 
-# I want to find negative numbers in the column.
-# there's only 215 of them, so I won't be able to see them.
+# find only negative numbers in the column.
+# only 12 lonely pixels below sea level
 has.neg <- apply(campus_DEM_df, 1, function(campus_DEM_df) any(campus_DEM_df < 0))
 length(which(has.neg))
 
+# challenge:
+# how many below 3.1 feet (1 m)?
+has.neg <- apply(campus_DEM_df, 1, function(campus_DEM_df) any(campus_DEM_df < 3.12))
+length(which(has.neg))
 
+# bonus challenge
+# can we map those?
 
 #############################################
 # ep. 2
