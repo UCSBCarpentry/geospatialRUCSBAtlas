9db5ae9938b4b9855c8ab5a90f22c52acc961f89
jonjab <jonjab@ucsb.edu>
2023-09-19 17:10:40 -0700

    separated out ep 8. ep 7 heavily edited.

--- scripts/ep_7-8.R
+++ scripts/ep_7-8.R
@@ -1,42 +1,76 @@
 #############################################
-# ep 7 and on w/ UCSB vector data
+# geospatial R UCSB examples
+# ep 7 : visualizing by attribute
+# and 8 : overlays
 
 library(tidyverse)
 library(rgdal)
 library(RColorBrewer)
 library(sf)
 
+
+# do we need to remake our objects?
 buildings <- st_read("source_data/Campus_Buildings/Campus_Buildings.shp")
 
-# count the objects and attributes
+# check out the attributes
 st_geometry_type(buildings)
+colnames(buildings)
 
 # see the CRS
 st_crs(buildings)
 
-# do it fast
+
+# 2 different ages in the table.
+# one is a string. one is a number.
+unique(buildings$date_bldg)
+unique(buildings$bld_date)
+
+# but how do we get at those attributes?
 plot(buildings)
 
 # do it pretty
 # by default we get a graticule
+# but how do we get at those attributes?
 ggplot() +
-  geom_sf(data = buildings, size = 0.1, color = 'black', fill = "gray") +
+  geom_sf(data = buildings) +
   ggtitle("Campus Buildings")
 
-# setting variables
-signs <- st_read("source_data/Coal_Oil_Sign_Inventory/Inventory.shp")
+ggplot() +
+  geom_sf(data = buildings, aes(bld_date)) +
+  ggtitle("Campus Buildings")
+
+
+
+# A: you could do it with a filter 
+
+# B: and then plot one-by-one layer
+
+# C: with an aes()
+# this one gives a gradient
+ggplot() +
+  geom_sf(data = buildings, aes(color = bld_date), size = 1.5) +
+  ggtitle("Campus Buildings", subtitle = "by age") +
+  coord_sf()
+
+# this one gives distinct colors
+ggplot() +
+  geom_sf(data = buildings, aes(color = factor(bld_date)), size = 1.5) +
+  ggtitle("Campus Buildings", subtitle = "by age") +
+  coord_sf()
+
+
+
+# We will want to bin that somehow, just like we
+# binned the campus elevation dataset
+
+# an example more analagous to the lesson would be 
+# bird types as a categorical variable.
+
+
+# more objects to map
 birds <- st_read("source_data/NCOS_Bird_Observations_20190619_web/NCOS_Bird_Observations_20190619_web.shp")
 habitat <- st_read("source_data/NCOS_Shorebird_Foraging_Habitat/NCOS_Shorebird_Foraging_Habitat.shp")
 
-#graphing variables
-ggplot() +
-  geom_sf(data = signs, size = 0.1, color = 'black', fill = "cyan1")
-
-ggplot() +
-  geom_sf(data = birds, size = 0.1, color = 'black', fill = "cyan1")
-
-ggplot() +
-  geom_sf(data = habitat, size = 0.1, color = 'black', fill = "cyan1")
 
 # hey, something to explore
 # the output of this shows 10 
@@ -49,7 +83,6 @@ levels(habitat$Elev_Range)
 
 
 # filter on attributes
-names(signs)
 names(birds)
 
 # 9 signs need to be fixed.
@@ -64,35 +97,4 @@ ggplot () +
   labs(color = 'Condition') +
   coord_sf()
 
-#############################################
-# ep. 8
-
-
-# add multiple geometries.
-names(birds)
-
-campus_DEM <- raster("source_data/greatercampusDEM/greatercampusDEM_1_1.tif")
-campus_DEM_df <- as.data.frame(campus_DEM, xy=TRUE)
-
-ggplot() +
-  geom_raster(data = campus_DEM_df, 
-              aes(x=x, y=y, fill = greatercampusDEM_1_1)) +
-  scale_fill_gradient2(na.value = "lightgrey", 
-                       low="black", 
-                       mid="azure1", 
-                       high="cornsilk3",
-                       midpoint=0) +
-  geom_sf(data = signs) +
-  geom_sf(data = birds) +
-  geom_sf(data = habitat) +
-  geom_sf(data = buildings)
-
-ggplot() +
-  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = greatercampusDEM_1_1)) +
-  scale_fill_gradient2(na.value = "lightgrey", 
-                       low="black", 
-                       mid="azure1", 
-                       high="cornsilk3",
-                       midpoint=0) +
-  coord_quickmap()
 
