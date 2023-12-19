3eccf74f112b5bb96138c6d29c050d0c56708c3a
Jon Jablonski <jjablonski@library.ucsb.edu>
2023-12-19 11:03:29 -0800

    adding outline info for 3 hillshades

--- scripts/map7.r
+++ scripts/map7.r
@@ -1,7 +1,21 @@
 # map 7
 # the zoom to Cali
 
-libraty(terra)
+library(terra)
 
+# full campus extent hillshade made from 
+# campus_DEM
+campus_DEM <- rast("output_data/campus_DEM.tif") 
+
+
+
+# find intermediate data for bite of Cali hillshade
+# use extent of campus_DEM as the overlaid locator
+campusExtent <- ext(campus_DEM)
+
+# crop this to a generous California 
+# and figure out the color scale
 world <- rast("downloaded_data/GRAY_50M_SR_OB.tif")
 plot(world, col="gray")
+
+
