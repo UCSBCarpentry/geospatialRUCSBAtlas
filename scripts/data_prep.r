93cc76c9761c71fa88b547be336b2c92ef28f269
Julien Brun <brunj7@users.noreply.github.com>
2024-01-16 12:14:38 -0800

    adding the download of UCSB building data

--- scripts/data_prep.r
+++ scripts/data_prep.r
@@ -9,6 +9,7 @@ library(terra)
 # curl is going to be better than download.file
 library(curl)
 library(googledrive)
+library(tidyverse)
 
 # Get Campus Rasters
 # **********************
@@ -86,6 +87,22 @@ download.file("https://drive.google.com/drive/folders/1XoOOD3xcTaSevQZGtwB9ndIwa
 
 # POLYGONS
 # Buildings (good extent example?)
+
+building_dir_url = "https://drive.google.com/drive/folders/1SwcCrBoa0a7I_kmBNCa3_zNQ6Aw9P-8H"
+building_dir = drive_get(building_dir_url)
+
+# create local dir
+dir_local <- file.path("source_data",  tolower(building_dir$name))
+dir.create(dir_local, showWarnings = FALSE)
+
+# find files in folder on GD
+files = drive_ls(building_dir)
+files_bind <- bind_rows(files)
+
+# Batch download the files
+map2(files_bind$id, files_bind$name, ~drive_download(as_id(.x), path = file.path(dir_local, .y)))
+
+
 # Foraging Habitat?
 # AOI's (Can be used later for clipping extents)
 #       (You should create them in this script)
