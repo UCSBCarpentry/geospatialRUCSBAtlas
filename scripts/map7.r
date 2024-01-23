0bdd6966c63c112d7be65699e0e25f6ce25a7f91
kristi-sara <ksliu@ucsb.edu>
2024-01-23 10:58:52 -0800

    having trouble with hillshades being created

--- scripts/map7.r
+++ scripts/map7.r
@@ -160,25 +160,29 @@ par(mfrow = c(1,1))
 
 # zoom3
 # figure out the layer names
+# kristi stuck here, cant get hillshades to load 
 (campus_DEM)
+#campus_hillshade <- rast("source_data/campus_hillshade.tif")
 (campus_hillshade)
 
 zoom3 <- ggplot() +
   geom_raster(data = campus_DEM,
-              aes(x=x, y=y, fill=layer)) +
+              aes(x=x, y=y, fill=layer) +
   geom_raster(data=campus_hillshade,
               aes(x=x, y=y, alpha = hillshade)) +
   scale_fill_viridis_c() +
   scale_alpha(range = c(0.15, 0.65), guide="none")
+plot(zoom3)
 
 # zoom2
+# isn't zoom 2 already a hillshade and should we be loading the cropped? 
 socal_hillshade
-cali_zoom_2
+(zoom_2_cropped)
 
 ggplot() +
-  geom_raster(data = cali_zoom_2,
+  geom_raster(data = zoom_2_cropped,
               aes(x=x, y=y, fill=dem90_hf)) +
-  geom_raster(data=socal_hillshade,
+  geom_raster(data=zoom_2_cropped,
               aes(x=x, y=y, alpha = hillshade)) +
   scale_fill_viridis_c() +
   scale_alpha(range = c(0.15, 0.65), guide="none")
