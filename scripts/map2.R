40d8862eaae596d4e23f27685fdbe7304b588f01
Jon Jablonski <jjablonski@library.ucsb.edu>
2024-07-09 11:37:16 -0700

    trees and bikes will plot() but not ggplot()

--- scripts/map2.R
+++ scripts/map2.R
@@ -1,6 +1,8 @@
 # map 2
 # Stylized trees, bikeways, walkways, and water
 # water is yet to be defined.
+# core skills from the lesson are in ep. 7
+
 
 # use to demo a more exotic ggplot(theme=)
 
@@ -8,4 +10,33 @@ library(tidyverse)
 library(raster)
 library(sf)
 
-trees <- st_read("source_data/trees/)
+
+trees <- vect("source_data/trees/DTK_012116.shp")
+plot(trees)
+names(trees)
+
+summary(trees)
+summary(trees$SPP)
+unique(trees$SPP)
+
+summary(trees$HT)
+unique(trees$HT)
+
+# let's make a size vector
+tree_height <- unique(trees$HT)
+
+# ggplot with points
+# sized by height
+ggplot() +
+  geom_point(data = trees, aes(x=x, y=y))
+
+
+
+
+
+
+
+bikes <- vect("source_data/bike_paths/bikelanescollapsedv8.shp")
+plot(bikes)  
+ggplot() +
+  geom_sf(data = bikes)
