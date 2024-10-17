2ad9bd006284da9e011f93d9f79b0fca6efce6f5
Jon Jablonski <jjablonski@library.ucsb.edu>
2024-10-17 09:21:51 -0700

    first commit for map / episode 12

---
+++ scripts/map 12.R
@@ -0,0 +1,22 @@
+# map 12
+# let's build monthly NDVI's for campus
+# as in episode 12 
+
+# loop over the files and build a raster stack
+
+# calculate the NDVIs
+
+# display the grid of NDVIs
+
+# make bins
+
+# display the binned histograms of the NDVIs
+
+# Julian dates: that's in the lesson, but ours uses calendar dates
+# challenge: change object names to Julian dates
+
+# What month was the Greenest?
+
+# we'll need weather data
+
+# when was it rainiest?
