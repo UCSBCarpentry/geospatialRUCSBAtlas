55f25b8acf831d9d997cd77991a2b1aadbcbafc4
Jon Jablonski <jjablonski@library.ucsb.edu>
2024-10-17 15:53:03 -0700

    a strong start to looping over images and creating NDVI's

--- scripts/map 12.R
+++ scripts/map 12.R
@@ -2,9 +2,53 @@
 # let's build monthly NDVI's for campus
 # as in episode 12 
 
+
+library(terra)
+library(scales)
+library(tidyr)
+library(ggplot2)
+library(raster)
+
+
+# NDVIs were premade in the Carpentries lesson, but
+# we already know enough raster math to make our
+# own
+
+
+
+# make an NDVI for 1 file
+tiff_path <- c("source_data/UCSB_campus_23-24_psscene_analytic_8b_sr_udm2/PSScene/20230912_175450_00_2439_3B_AnalyticMS_SR_8b_clip.tif")
+
+#(NIR - Red) / (NIR + Red)
+
+image <- brick(tiff_path, n1=8)
+ndvi_tiff <- (image[[8]] - image[[6]] / image[[8]] + image[[6]])
+plot(ndvi_tiff)
+plotRGB(image, r=6,g=3,b=1, stretch = "hist")
+
+# load 23-24 8-band rasters
 # loop over the files and build a raster stack
 
-# calculate the NDVIs
+# get a file list
+scene_paths <- list.files("source_data/UCSB_campus_23-24_psscene_analytic_8b_sr_udm2/PSScene",
+                          full.names = TRUE,
+                          pattern = "8b_clip.tif")
+scene_paths[1]
+str(scene_paths)
+
+# someplace to put our images
+dir.create("output_data/ndvi", showWarnings = FALSE)
+
+# calculate the NDVIs loop
+for (images in scene_paths) {
+    source_image <- brick(images, n1=8)
+    ndvi_tiff <- (source_image[[8]] - source_image[[6]] / source_image[[8]] + source_image[[6]])
+    new_filename <- (substr(images, 67,92))
+    new_filename <- paste("output_data/ndvi/", new_filename, ".tif", sep="")
+    print(new_filename)
+    plot(ndvi_tiff)
+    writeRaster(ndvi_tiff, new_filename, filetype="GTiff", overwrite=TRUE)
+        }
 
 # display the grid of NDVIs
 
