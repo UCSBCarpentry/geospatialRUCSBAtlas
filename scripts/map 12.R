37c0ab36277e82dde19d466beed7e9a957e19eb9
Jon Jablonski <jjablonski@library.ucsb.edu>
2024-10-18 14:46:05 -0700

    pre-pull commit of extend futzhing.

--- scripts/map 12.R
+++ scripts/map 12.R
@@ -8,7 +8,7 @@ library(scales)
 library(tidyr)
 library(ggplot2)
 library(raster)
-library(sf) <- to handle geojson
+# library(geojsonsf) # to handle geojson
 
 
 # NDVIs were premade in the Carpentries lesson, but
@@ -22,7 +22,12 @@ tiff_path <- c("source_data/UCSB_campus_23-24_psscene_analytic_8b_sr_udm2/PSScen
 
 #(NIR - Red) / (NIR + Red)
 
+# brick is raster. rast is terra
+# the ndvi looks VERY different
 image <- brick(tiff_path, n1=8)
+image <- rast(tiff_path)
+
+
 ndvi_tiff <- (image[[8]] - image[[6]] / image[[8]] + image[[6]])
 plot(ndvi_tiff)
 plotRGB(image, r=6,g=3,b=1, stretch = "hist")
@@ -42,20 +47,30 @@ dir.create("output_data/ndvi", showWarnings = FALSE)
 
 # We need a common extent to make
 # a raster stack
-ucsb_extent <- st_read("source_data/ucsb_60sqkm_planet_extent.geojson")
-ucsb_extent <- extent(ucsb_extent)
+ucsb_extent <- vect("source_data/ucsb_60sqkm_planet_extent.geojson")
+str(ucsb_extent)
+crs(ucsb_extent)
+crs(ndvi_tiff)
 
+# this tests to see if we can take our calculated NDVIs
+# and reproject them to the CRS of our AOI
+ucsb_extent <- project(ucsb_extent, ndvi_tiff)
+
+# can I also extend it?
+# yes I can.
+ndvi_tiff <- extend(ndvi_tiff, ucsb_extent)
+plot(ndvi_tiff)
 
 # calculate the NDVIs 
 # and set them to the same extent
 # loop
 for (images in scene_paths) {
-    source_image <- brick(images, n1=8)
+    source_image <- rast(images)
     ndvi_tiff <- (source_image[[8]] - source_image[[6]] / source_image[[8]] + source_image[[6]])
     new_filename <- (substr(images, 67,92))
     new_filename <- paste("output_data/ndvi/", new_filename, ".tif", sep="")
-    setExtent(ndvi_tiff, ucsb_extent) 
     print(new_filename)
+    ndvi_tiff <- extend(ndvi_tiff, ucsb_extent)
     plot(ndvi_tiff)
     writeRaster(ndvi_tiff, new_filename, filetype="GTiff", overwrite=TRUE)
         }
@@ -65,10 +80,11 @@ for (images in scene_paths) {
 # # get a list of the new files:
 ndvi_series_names <- list.files("output_data/ndvi")
 ndvi_series_names <- paste("output_data/ndvi/", ndvi_series_names, sep="")
-(ndvi_series_names)
+length(ndvi_series_names)
 
-# build raster stack
+length()# build raster stack
 ndvi_series_stack <- rast(ndvi_series_names)
+ndvi_series_stack <- brick(ndvi_series_names, n1=23)
 
 
 # make bins
