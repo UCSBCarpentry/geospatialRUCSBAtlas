089a149d9c7ab87e1f16c912dda657470bdc49c2
Jon Jablonski <jjablonski@library.ucsb.edu>
2024-11-05 13:45:02 -0800

    Got map 7 to draw mostly as map 1

--- scripts/map7.r
+++ scripts/map7.r
@@ -19,11 +19,11 @@ bikeways <- st_read("source_data/bike_paths/bikelanescollapsedv8.shp")
 habitat <- st_read("source_data/NCOS_bird_observations/NCOS_Shorebird_Foraging_Habitat.shp")
 
 # rasters
-# the background setup is bathymetry and topography
-# mashed together
+# the background setup is bathymetry and topography mashed together
 
-campus_DEM <- rast("output_data/campus_DEM.tif") 
+campus_DEM <- rast("source_data/campus_DEM.tif") 
 campus_bath <- rast("output_data/campus_bath.tif")
+campus_hillshade <- rast("source_data/campus_hillshade.tif")
 
 # We'll need some bins
 # best coast line bins from ep 2
@@ -33,7 +33,13 @@ custom_bins <- c(-3, 4.9, 5, 7.5, 10, 25, 40, 70, 100, 150, 200)
 # we will use the original projection of
 #    campus_DEM
 campus_projection <- crs(campus_DEM)
+campus_bath <- project(campus_bath, campus_projection)
+campus_hillshade <- project(campus_hillshade, campus_projection)
+buildings <- st_transform(buildings, campus_projection)
+bikeways <- st_transform(bikeways, campus_projection)
+habitat <- st_transform(habitat, campus_projection)
 
+crs(buildings)
 
 # make dataframes
 campus_DEM_df <- as.data.frame(campus_DEM, xy=TRUE) %>%
@@ -41,6 +47,8 @@ campus_DEM_df <- as.data.frame(campus_DEM, xy=TRUE) %>%
 
 campus_bath_df <- as.data.frame(campus_bath, xy=TRUE) %>%
   rename(bathymetry = SB_bath_2m)
+
+campus_hillshade_df <- as.data.frame(campus_hillshade, xy=TRUE)
 
 # to make our scales make sense, we do 
 # raster math 
@@ -53,28 +61,32 @@ sea_level_0 <- app(sea_level, function(x) ifelse(x <=0, NA, x))
 # we are going to want those back later as our 'vernal pools'
 
 
-
-
-# load campus_bathymetry raster from output folder
-# remember: this is the one we cropped, so the 2 extents are the same.
-# not sure exactlty where this gets made
-campus_bath <- rast("output_data/campus_bath.tif")
-
-
-
-
-#update color scheme for contrast 
-# +hillshade
+# test vector overlays
 ggplot() +
-  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = elevation)) +
-  geom_raster(data = campus_hillshade_df, aes(x=x, y=y, alpha = campus_hillshade), show.legend = FALSE) +
-  geom_raster(data = campus_bath_df, aes(x=x, y=y, fill = bathymetry)) +
-  scale_fill_viridis_c(na.value="NA") +
-  geom_sf(data=buildings, color ="hotpink") +
-  geom_sf(data=habitat, color="darkorchid1") +
-  geom_sf(data=bikeways, color="yellow") +
-  labs(title="Map 1", subtitle="Version 3") +
+  geom_sf(data=habitat) +
+  geom_sf(data=buildings) +
+  geom_sf(data=iv_buildings) +
+  geom_sf(data=bikeways) +
   coord_sf()
 
-ggsave("images/map1.3.png", plot=last_plot())
+
+
+
+############################
+str(campus_hillshade_df)
+
+# this is mostly map 1 so far
+
+ggplot() +
+  geom_raster(data = campus_DEM_df, aes(x=x, y=y, fill = elevation)) +
+  geom_sf(data=buildings, color ="hotpink") +
+  geom_sf(data=bikeways, color="yellow") +
+  geom_raster(data = campus_hillshade_df, aes(x=x, y=y, alpha = hillshade), show.legend = FALSE) +
+  geom_sf(data=habitat, color="darkorchid1") +
+  geom_raster(data = campus_bath_df, aes(x=x, y=y, fill = bathymetry)) +
+  scale_fill_viridis_c(na.value="NA") +
+  labs(title="Map 7", subtitle="Version 1") +
+  coord_sf()
+
+ggsave("images/map7.0.png", plot=last_plot())
 object_test_abb <- ls()
