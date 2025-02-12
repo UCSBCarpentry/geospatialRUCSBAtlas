04d1d358074b1f966a4e8f3fd215871d9c6f3340
Jon Jablonski <jjablonski@library.ucsb.edu>
2025-02-12 12:56:42 -0800

    re-establishing the narrative for these, and making them run.

--- scripts/ep_13.R
+++ scripts/ep_13.R
@@ -12,22 +12,27 @@ library(ggplot2)
 library(dplyr)
 library(sf)
 
-#recreate necessary objects for map 1
-#possible take the reprojections from map 4-5-6 instead
-# - campus_crs,campus_crs (formerly my_crs)
-
+#recreate necessary objects from map 1
 campus_DEM <- rast("source_data/campus_DEM.tif") 
 crs(campus_DEM)
+
+campus_projection <- crs(campus_DEM)
+
+# shouldn't we be making the NDVI graph here first?
+# that's how episode 13 starts.
+
 
 campus_DEM_df <- as.data.frame(campus_DEM, xy=TRUE) %>%
   rename(elevation = greatercampusDEM_1_1) # rename to match code later
 str(campus_DEM_df)
 
-#load in campus_bath made in map 1
-campus_bath <- rast("output_data/campus_bath.tif")
+#load in campus_bath made in episode 3
+campus_bath <- rast("output_data/ep_3_campus_bathymetry_crop.tif")
+(campus_bath)
 
 campus_bath_df <- as.data.frame(campus_bath, xy=TRUE) %>%
-  rename(bathymetry = SB_bath_2m)
+  rename(bathymetry = Bathymetry_2m_OffshoreCoalOilPoint)
+
 str(campus_bath_df)
 
 crs(campus_DEM) == crs(campus_bath)
@@ -43,9 +48,9 @@ str(campus_hillshade_df)
 #load and reproject vectors
 
 buildings <- st_read("source_data/campus_buildings/Campus_Buildings.shp")
-iv_buildings <- st_read("source_data/iv_buildings/CA_Structures_ExportFeatures.shp")
-bikeways <- st_read("source_data/bike_paths/bikelanescollapsedv8.shp")
-habitat <- st_read("source_data/NCOS_bird_observations/NCOS_Shorebird_Foraging_Habitat.shp")
+iv_buildings <- st_read("source_data/iv_buildings/iv_buildings/CA_Structures_ExportFeatures.shp")
+bikeways <- st_read("source_data/icm_bikes/bike_paths/bikelanescollapsedv8.shp")
+habitat <- st_read("source_data/NCOS_Shorebird_Foraging_Habitat/NCOS_Shorebird_Foraging_Habitat.shp")
 
 buildings <- st_transform(buildings, campus_projection)
 iv_buildings <- st_transform(iv_buildings, campus_projection)
@@ -72,42 +77,26 @@ ggplot() +
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ############Narrative
 
 # facet wrapped NDVIs from ep. 12
+
+# make a list of all your tiffs
+# they get made by map 12. 
+ndvi_series_names <- list.files("output_data/ndvi")
+ndvi_series_path <- paste("output_data/ndvi/", ndvi_series_names, sep="")
+
+# path to files:
+# now we can build a raster stack with no errors
+ndvi_series_stack <- rast(ndvi_series_path)
+
+summary(ndvi_series_stack[,1])
+
+
+
+
+
+
 
 # formatted map of same
 
