5842004b355ac6dc6e0a4657cea3c3193c89619d
Jon Jablonski <jjablonski@library.ucsb.edu>
2025-02-27 10:58:56 -0800

    working the narrative back from episode 11 to 7. file cleanup from splitting ep 7 and 8

--- scripts/ep_10.r
+++ scripts/ep_10.r
@@ -1,11 +1,37 @@
 # episode 10
 # Convert from .csv to a Vector Layer
 
-# should we concentrate on json? we use that elsewhere.
+# we will also use a json aoi as we use that elsewhere.
 
 # or make a csv from existing data and then backwards convert it?
 
+library(sf)
+library(terra)
 
-# ends with st_write out to a shapefile.
-# from this point on, we should depend more on 
+# Import .csv
+
+# Identify x,y, columns
+# head()
+
+# .csv to sf object
+
+# st_as_sf() 
+
+
+
+# Plot Spatial Object
+
+# Plot Extent
+# AOI plus points
+
+# use a geojson aoi
+
+
+# Challenge: import & plot more points
+
+# Export to an Esri shapefile
+
+
+# ends with st_write out to a point shapefile.
+# from this point on, maybe we should depend more on 
 # the output data folder.
