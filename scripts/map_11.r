adca2afe78a4fb23f5da4b233a32f25a72ffc435
Jon Jablonski <jjablonski@library.ucsb.edu>
2025-03-07 12:57:27 -0800

    cropping map 11 with seth's corner coordinate solution

--- scripts/map_11.r
+++ scripts/map_11.r
@@ -22,3 +22,18 @@ str(collar_df)
 
 collar_vect <- vect(collar)
 ## Jon is thinking this is a georeferencing table -- not a definition of the collar
+
+# Crop extent from lat/long on the map corners:
+# NW corner: 34° 30' 00" N (34.500) / 119° 52' 30" W (-119.875) 
+# NE corner: 34° 30' 00" N (34.500) / 119° 45' 00" W (-119.750)
+# SE corner: 34° 22' 30" N (34.375) / 119° 45' 00" W (-119.750)
+# SW corner: 34° 22' 30" N (34.375) / 119° 52' 30" W (-119.875) 
+ll_ext <- ext(-119.875, -119.750, 34.375, 34.500)
+
+# make a vector to reproject form lat/long to utm 
+ll_vec <- as.polygons(ll_ext, crs="EPSG:4326")
+utm_ext <- ext(project(ll_vec, "EPSG:32611"))
+
+# do the crop
+crop_dibblee_gol <- crop(dibblee_gol, utm_ext)
+plotRGB(crop_dibblee_gol)
