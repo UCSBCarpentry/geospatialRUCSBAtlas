f7823d02d85426dcdca8c88584013d831d551672
Jose Nino <jose_nino@ucsb.edu>
2025-03-11 09:59:54 -0700

    first try on map 3

--- scripts/map_03.r
+++ scripts/map_03.r
@@ -4,16 +4,20 @@
 # and map 7, which is a version of map 1
 # 
 
-# set map number
-current_sheet <- 3
-# set ggplot counter
-current_ggplot <- 0
+# Load magick package, which allows to make a montage of images
+library(magick)
 
-gg_labelmaker <- function(plot_num){
-  gg_title <- c("Map:", current_sheet, " ggplot:", plot_num)
-  plot_text <- paste(gg_title, collapse=" " )
-  print(plot_text)
-  current_ggplot <<- plot_num
-  return(plot_text)
-}
+# Load maps 4, 5, 6, and 7 using the magick::image_read function
+map4 <- image_read("images/map4.png")
+map5 <- image_read("images/map5.png")
+map6 <- image_read("images/map6.png")
+map7 <- image_read("images/map7.0.png")
 
+# make a vector of the maps, and then make the montage of map 3
+input <- c(map4, map5, map6, map7)
+map3 <- image_montage(image = input,
+              tile = "3x2",
+              geometry = 'x700')
+
+#save map3
+image_write(map3, "images/map3.png", format = "png")
